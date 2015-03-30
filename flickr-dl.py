8bfd75182b4721b3e40eb18c5736226f416ee6c6
Eshin Kunishima <omochi@luna.miko.im>
2015-03-31 08:28:16 +0900

    Add arguments parser

--- flickr-dl.py
+++ flickr-dl.py
@@ -1,33 +1,41 @@
+__author__ = 'Eshin Kunishima'
 __license__ = 'MIT'
 
 import sys
 import json
 import urllib.request
 import urllib.parse
+import argparse
 
 api_url = 'https://api.flickr.com/services/rest/?'
-api_key = 'TYPE YOUR API KEY'
-
-user_id = 'TYPE YOUR VICTIM'
 
 
 def main():
     page = 1
     page_max = 999
 
+    parser = argparse.ArgumentParser(description='The photo urls collector for Flickr written by Python.')
+    parser.add_argument('api_key', type=str, help='API key')
+    parser.add_argument('user_id', type=str, help='Target username')
+    arguments = parser.parse_args()
+
+    api_key = arguments.api_key
+    user_id = arguments.user_id
+
     while page <= page_max:
         print('{:d}/{:d}'.format(page, page_max), file=sys.stderr)
 
         request = urllib.request.Request(api_url + urllib.parse.urlencode(
             {'api_key': api_key, 'method': 'flickr.people.getPhotos', 'user_id': user_id,
-             'extras': 'url_m,url_z,url_c,url_l,url_h,url_k,url_o', 'per_page': '500', 'page': page,
-             'format': 'json', 'nojsoncallback': 1}))
+             'extras': 'url_sq,url_q,url_t,url_s,url_n,url_m,url_z,url_c,url_l,url_h,url_k,url_o', 'per_page': '500',
+             'page': page, 'format': 'json', 'nojsoncallback': 1}))
         request.add_header('User-Agent', 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)')
         response = urllib.request.urlopen(request)
 
         root = json.loads(response.read().decode(encoding='utf-8'))
 
         if not root['stat'] == 'ok':
+            print(root['message'])
             return False
 
         page_max = root['photos']['pages']
@@ -55,8 +63,11 @@ def main():
                 print(photo['url_t'])
             elif 'url_q' in photo:  # Large Square
                 print(photo['url_q'])
-            elif 'url_sq' in photo: # Square
+            elif 'url_sq' in photo:  # Square
                 print(photo['url_sq'])
+            else:
+                print(photo)
+                page_max = 0
 
         page += 1
 
